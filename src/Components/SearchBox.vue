fd0c8c4040c4185231c1b90ef80f62d8b335d742
Sheila Jagla <sheilajagla@gmail.com>
2025-09-18 07:49:53 -0600

    updates search box focus state

--- src/Components/SearchBox.vue
+++ src/Components/SearchBox.vue
@@ -27,7 +27,7 @@ defineProps(["modelValue"]);
         @input="$emit('updateSearch', $event.target.value)"
         type="text"
         placeholder="Search tenants by name..."
-        class="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+        class="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-gray-300 focus:border-gray-400 sm:text-sm"
       />
     </div>
   </div>
